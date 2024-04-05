794631f19246951f3dbf937c3a7ee2ab18316596
Jackson Neal <jackson.neal@comcast.net>
2024-04-05 17:27:36 -0600

    more stuff
    more stuff

--- Assets/Scripts/LightControiller.cs
+++ Assets/Scripts/LightControiller.cs
@@ -4,15 +4,53 @@ using UnityEngine;
 
 public class LightControiller : MonoBehaviour
 {
+    private Light light;
+
+    bool isTouching = false;
+
+    public static int lightLifespan = 60000;
+    private int lightHalfLife = lightLifespan / 2;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        //mainObject = transform.GetChild(0).gameObject;
+        light = GetComponent<Light>();
+        light.intensity = 0.0f;
+        //light.enabled = false;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        lightLifespan--;
+        if (isTouching)
+        {
+            light.intensity = 10.0f;
+        }
+
+
+        if (lightLifespan <= lightHalfLife && lightLifespan > 0)
+        {
+            light.intensity = 5.0f;
+        }
+        else if (lightLifespan <= 0)
+        {
+            light.enabled = false;
+            light.intensity = 0.0f;
+            //mainObject.SetActive(false);
+            Destroy(gameObject, 0.1f);
+        }
+
     }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            isTouching = true;
+        }
+        //else isTouching= false;
+    }
+
 }
